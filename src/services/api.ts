276835339648b85165aac36223e2dd843b37a232
ErinKace <erinkacerovskis@gmail.com>
2025-07-23 15:43:02 -0500

    working on updating api service for security

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
-const API_KEY = "8a485baae5ae1f5408a0e4a7402542e1";
+const apiKey = process.env.REACT_APP_TMDB_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${apiKey}`);
     const data = await response.json()
     return data.results
 };
