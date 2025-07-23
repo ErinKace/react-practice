13175248dd88895dafde4e7da771f275eb14d50e
ErinKace <erinkacerovskis@gmail.com>
2025-07-23 16:23:35 -0500

    fixed api service error and created search function

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,14 @@
-const apiKey = process.env.REACT_APP_TMDB_KEY;
+const apiKey = import.meta.env.VITE_TMDB_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${apiKey}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query: any) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`);
     const data = await response.json()
     return data.results
 };
